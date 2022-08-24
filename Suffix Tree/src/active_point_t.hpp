0ff848c0cdc4201d08e0d217148cad7d6366831e
pelirodri <pelirodri@icloud.com>
2022-08-24 16:53:35 -0400

    Perform minor changes

--- Suffix Tree/src/active_point_t.hpp
+++ Suffix Tree/src/active_point_t.hpp
@@ -2,11 +2,16 @@
 #define ACTIVE_POINT_T_HPP
 	namespace st {
 		struct active_point_t {
-			active_point_t(suffix_tree_node_t *node = nullptr, unsigned char edge = -1, size_t length = 0) :
+			active_point_t(suffix_tree_node_t *node = nullptr, unsigned char edge = 0, size_t length = 0) :
 				node(node), edge(edge), length(length) {}
 
-			active_point_t(active_point_t *active_point) :
-				node(active_point->node), edge(active_point->edge), length(active_point->length) {}
+			~active_point_t() = default;
+
+			active_point_t(const active_point_t &other) : node(other.node), edge(other.edge), length(other.length) {}
+			active_point_t &operator=(const active_point_t &other) = delete;
+
+			active_point_t(active_point_t &&other) noexcept = delete;
+			active_point_t &operator=(active_point_t &&other) noexcept = delete;
 
 			suffix_tree_node_t *node;
 			unsigned char edge;
--- Suffix Tree/src/suffix_tree_node_t.hpp
+++ Suffix Tree/src/suffix_tree_node_t.hpp
@@ -4,6 +4,30 @@
 	#include <array>
 
 	namespace st {
-		
+		struct suffix_tree_node_t {
+			suffix_tree_node_t(int start = -1, int *end = nullptr) :
+				start(start), end(end), suffix_link(nullptr), suffix_index(-1) {}
+
+			~suffix_tree_node_t() {
+				if (end && suffix_index == -1) {
+					delete end;
+				}
+			}
+
+			suffix_tree_node_t(suffix_tree_node_t &&) noexcept = default;
+			suffix_tree_node_t &operator=(suffix_tree_node_t &&) noexcept = default;
+
+			int start;
+			int *end;
+
+			std::array<std::unique_ptr<suffix_tree_node_t>, 256> children;
+			suffix_tree_node_t *suffix_link;
+
+			int suffix_index;
+
+			inline size_t get_edge_length() const {
+				return end ? (*end - start) + 1 : 0;
+			}
+		};
 	}
 #endif
